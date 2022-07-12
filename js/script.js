5f1fe818a1bfb232f776e3096080d7529f5bdf58
mdsakibhossen <sakib.hossen0143@gmail.com>
2022-07-13 00:56:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,129 @@
+// Button 02 Start
+const menu_btn_02 = document.querySelector('.menu_btn_02');
+const bar_02 = menu_btn_02.children;
+menu_btn_02.addEventListener('click',()=>{
+    const bar1_02 = bar_02[0];
+    const bar2_02 = bar_02[1];
+    const bar3_02 = bar_02[2];
+
+    bar1_02.classList.toggle('bar1_02');
+    bar2_02.classList.toggle('bar2_02');
+    bar3_02.classList.toggle('bar3_02');
+})
+// Button 02 End
+
+
+// Button 03 Start
+const menu_btn_03 = document.querySelector('.menu_btn_03');
+const bar_03 = menu_btn_03.children;
+menu_btn_03.addEventListener('click', () => {
+    const bar1_03 = bar_03[0];
+    const bar2_03 = bar_03[1];
+    const bar3_03 = bar_03[2];
+
+    bar1_03.classList.toggle('bar1_03');
+    bar2_03.classList.toggle('bar2_03');
+    bar3_03.classList.toggle('bar3_03');
+})
+// Button 03 End
+
+
+// Button 04 Start
+const menu_btn_04 = document.querySelector('.menu_btn_04');
+const bar_04 = menu_btn_04.children;
+menu_btn_04.addEventListener('click', () => {
+    const bar1_04 = bar_04[0];
+    const bar2_04 = bar_04[1];
+    const bar3_04 = bar_04[2];
+
+    bar1_04.classList.toggle('bar1_04');
+    bar2_04.classList.toggle('bar2_04');
+    bar3_04.classList.toggle('bar3_04');
+})
+// Button 04 End
+
+// Button 05 Start
+const menu_btn_05 = document.querySelector('.menu_btn_05');
+const bar_05 = menu_btn_05.children;
+menu_btn_05.addEventListener('click', () => {
+    const bar1_05 = bar_05[0];
+    const bar2_05 = bar_05[1];
+    const bar3_05 = bar_05[2];
+
+    bar1_05.classList.toggle('bar1_05');
+    bar2_05.classList.toggle('bar2_05');
+    bar3_05.classList.toggle('bar3_05');
+})
+// Button 05 End
+
+// Button 06 Start
+const menu_btn_06 = document.querySelector('.menu_btn_06');
+const bar_06 = menu_btn_06.children;
+menu_btn_06.addEventListener('click', () => {
+    const bar1_06 = bar_06[0];
+    const bar2_06 = bar_06[1];
+    const bar3_06 = bar_06[2];
+
+    bar1_06.classList.toggle('bar1_06');
+    bar2_06.classList.toggle('bar2_06');
+    bar3_06.classList.toggle('bar3_06');
+})
+// Button 06 End
+
+
+// Button 07 Start
+const menu_btn_07 = document.querySelector('.menu_btn_07');
+const bar_07 = menu_btn_07.children;
+menu_btn_07.addEventListener('click', () => {
+    const bar1_07 = bar_07[0];
+    const bar2_07 = bar_07[1];
+    const bar3_07 = bar_07[2];
+
+    bar1_07.classList.toggle('bar1_07');
+    bar2_07.classList.toggle('bar2_07');
+    bar3_07.classList.toggle('bar3_07');
+})
+// Button 07 End
+
+// Button 08 Start
+const menu_btn_08 = document.querySelector('.menu_btn_08');
+const bar_08 = menu_btn_08.children;
+menu_btn_08.addEventListener('click', () => {
+    const bar1_08 = bar_08[0];
+    const bar2_08 = bar_08[1];
+    const bar3_08 = bar_08[2];
+
+    bar1_08.classList.toggle('bar1_08');
+    bar2_08.classList.toggle('bar2_08');
+    bar3_08.classList.toggle('bar3_08');
+})
+// Button 08 End
+
+// Button 09 Start
+const menu_btn_09 = document.querySelector('.menu_btn_09');
+const bar_09 = menu_btn_09.children;
+menu_btn_09.addEventListener('click', () => {
+    const bar1_09 = bar_09[0];
+    const bar2_09 = bar_09[1];
+    const bar3_09 = bar_09[2];
+
+    bar1_09.classList.toggle('bar1_09');
+    bar2_09.classList.toggle('bar2_09');
+    bar3_09.classList.toggle('bar3_09');
+})
+// Button 09 End
+
+
+// Button 10 Start
+const menu_btn_10 = document.querySelector('.menu_btn_10');
+const bar_10 = menu_btn_10.children;
+menu_btn_10.addEventListener('click', () => {
+    const bar1_10 = bar_10[0];
+    const bar2_10 = bar_10[1];
+    const bar3_10 = bar_10[2];
+
+    bar1_10.classList.toggle('bar1_10');
+    bar2_10.classList.toggle('bar2_10');
+    bar3_10.classList.toggle('bar3_10');
+})
+// Button 10 End
